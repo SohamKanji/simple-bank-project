0819cbfdf559d46a43b6ee31a571376cba60457d
Soham Kanji <sohamkanji02@gmail.com>
2024-09-10 02:50:10 +0530

    implemented apis and basic api testing

---
+++ api/server.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	db "github.com/SohamKanji/simple-bank-project/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and set up routing.
+func NewServer(store db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// add routes to router
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+	router.PUT("/accounts/:id", server.updateAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ util/config.go
@@ -0,0 +1,28 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Conifg stores all configuration for the application
+// The values ate read by viper from config file or environment variables.
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig reads configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env") // we can aslo use json, yaml, toml, etc
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}

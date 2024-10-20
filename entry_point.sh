68b91a061e9ec7ce0143a24c888a2e1b3385289c
Soham Kanji <sohamkanji02@gmail.com>
2024-10-21 04:51:37 +0530

    Moved waiting for postgresql in entry_point.sh to compose

--- entry_point.sh
+++ entry_point.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Wait until PostgreSQL is ready
-echo "Waiting for PostgreSQL to be ready..."
-until pg_isready -h db -p 5432 -U root; do
-  sleep 1
-done
-
 # Run migrations using Golang Migrate
 source /app/app.env
 echo "Running database migrations..."
